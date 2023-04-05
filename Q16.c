13bc9ea27af9fb3f77c8f0c358b2940813944395
patelvansh1107 <patelvansh4750@gmail.com>
2023-04-05 08:36:21 +0530

    seven

--- Q16.c
+++ Q16.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 main()
 {
-	int n,i;
-	int sum;
-	
-	printf("Enter n : ");
-	scanf("%d",&n);
-	
-	while(n != 0){
-		sum = sum + i;
-	}
-	
-	printf("sum = %d",sum);
+int n,a;
+int sum = 0;
+ 
+printf("Enter the value of n :");    
+scanf("%d",&n); 
+ 
+while(n>0)    
+{    
+a = n % 10;    
+sum += a;   
+n = n / 10;    
+}   
+printf("Sum of digits is = %d",sum);    
 }
--- Q17.c
+++ Q17.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 main()
 {
+	int n, sum=0;
 	
+	printf("Enter the value of n : ");
+	scanf("%d",&n);
+	
+	int first,last;
+	
+	last = n % 10;
+	
+	while(n>9){
+		n /= 10;
+	}
+	first = n;
+	
+	sum = first + last;
+	
+	printf("sum = %d",sum);	
 }

bcd92dff40373b818db052471a36b36ac58e2254
patelvansh1107 <patelvansh4750@gmail.com>
2023-04-12 10:28:54 +0530

    vansh

--- Q18.c
+++ Q18.c
@@ -1,5 +1,25 @@
-#include<stdio.h>
-main()
-{
+#include <stdio.h>
+int main() {
 	
+  int n;
+  int i;
+  
+  int arr[100];
+  printf("Enter the number of elements : ");
+  scanf("%d", &n);
+
+  for (i = 0; i < n; ++i) {
+    printf("Enter number%d: ", i + 1);
+    scanf("%d", &arr[i]);
+  }
+
+  for (i = 1; i < n; ++i) {
+    if (arr[0] < arr[i]) {
+      arr[0] = arr[i];
+    }
+  }
+
+  printf("Largest element = %d", arr[0]);
+
 }
+
--- Q20.c
+++ Q20.c
@@ -1,5 +1,22 @@
-#include<stdio.h>
-main()
+# include <stdio.h>
+reverse(char *str)
 {
-	
+if (*str)
+{
+	reverse(str+1);
+	printf("%c", *str);
 }
+}
+
+
+int main()
+{
+char a[30];
+
+printf("Enter the string : ");
+gets(a);
+
+reverse(a);
+
+}
+

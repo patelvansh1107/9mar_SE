d037f15232c5392d5b181900ce27fbdde7af99af
patelvansh1107 <patelvansh4750@gmail.com>
2023-04-12 08:18:54 +0530

    vansh

--- Q10(3).c
+++ Q10(3).c
@@ -1,15 +1,31 @@
 #include<stdio.h>
 main()
 {
-//	datatype declaration
-	int i,num;
+	int i,n;
+	int ecount = 0;
+	int ocount = 0;
 	
-//	take input from user
-	printf("Enter the value of num : ");
-	scanf("%d",&num);
+	int esum = 0;
+	int osum = 0;
 	
-//	for loop declaration
-	for(i=0;i<=num;i=i+2){
-		printf("\n%d",i);
+	for(i=0;i<=10;i++)
+	{
+		printf("Enter the number : ");
+		scanf("%d",&n);
+		if(n%2==0)
+		{
+			ecount+=1;
+			esum+=n;
+		}
+		else
+		{
+			ocount+=1;
+			osum+=n;
+		}
 	}
+	printf("\n ecount = %d",ecount);
+	printf("\n ocount = %d",ocount);
+	
+	printf("\n esum = %d",esum);
+	printf("\n osum = %d",osum);
 }

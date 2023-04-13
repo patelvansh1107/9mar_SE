c42189e0e9f875abf65aafea0b2679909894c380
patelvansh1107 <patelvansh4750@gmail.com>
2023-04-13 10:55:05 +0530

    vansh

--- pattern(5).c
+++ pattern(5).c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main()
-{
-	
-}
 
+int main() {
+  int n;
+  int i,j,k;
+  
+  printf("Enter the value of n : ");
+  scanf("%d",&n);
+
+  
+  for(i = 0; i < n; i++) {
+    
+    for (j = 0; j < n-i-1; j++) {
+      printf(" ");
+    }
+    
+    for (k = 0; k < 2*i+1; k++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  
+}
--- pattern(6).c
+++ pattern(6).c
@@ -1,5 +1,24 @@
-#include<stdio.h>
-main()
-{
-	
+#include <stdio.h>
+main() {
+
+  int n;
+  int i,j,k;
+  
+  printf("Enter the value of n : ");
+  scanf("%d",&n);
+
+  for (i = 1; i <= n; i++) {
+    for (j = 0; j < i; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  
+  for (i = 1; i <= n-1; i++) {
+    for (j = 0; j < n-i; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  
 }
